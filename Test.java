ff7f780e594252ef69227b9d38b2862bb1bdd180
aatifaalam <95271893+aatifaalam@users.noreply.github.com>
2022-08-27 23:03:50 +0530

    my tenth commit

--- Test.java
+++ Test.java
@@ -5,3 +5,16 @@ public class Test {
     }
 }
 
+//Operator
+
+public class Main {
+  public static void main(String[] args) {
+    int sum1 = 300 + 78;
+    int sum2 = sum1 + 657;
+    int sum3 = sum2 + sum2;
+    System.out.println(sum1);
+    System.out.println(sum2);
+    System.out.println(sum3);  
+  }
+}
+
