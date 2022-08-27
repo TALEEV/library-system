c84dfa69dcefc008f2f1278c627105060fa857f9
aatifaalam <95271893+aatifaalam@users.noreply.github.com>
2022-08-27 23:00:27 +0530

    my ninth commit

--- Main.java
+++ Main.java
@@ -7,3 +7,22 @@ public class Main {
     System.out.println(myMethod(5, 3));
   }
 }
+
+// Another Program
+
+public class Main {
+ 
+  public void fullThrottle() {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+
+  public static void main(String[] args) {
+    Main myCar = new Main();
+    myCar.fullThrottle();
+    myCar.speed(200);
+  }
+}
