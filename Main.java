27d770fb99cfab5f83e1d18bf96c9c4137262a7b
aatifaalam <95271893+aatifaalam@users.noreply.github.com>
2022-08-27 22:42:09 +0530

    my eighth commit

--- Main.java
+++ Main.java
@@ -1,20 +1,9 @@
-// Create a Main class
 public class Main {
- 
-  // Create a fullThrottle() method
-  public void fullThrottle() {
-    System.out.println("The car is going as fast as it can!");
+  static int myMethod(int x, int y) {
+    return x + y;
   }
 
-  // Create a speed() method and add a parameter
-  public void speed(int maxSpeed) {
-    System.out.println("Max speed is: " + maxSpeed);
-  }
-
-  // Inside main, call the methods on the myCar object
   public static void main(String[] args) {
-    Main myCar = new Main();     // Create a myCar object
-    myCar.fullThrottle();      // Call the fullThrottle() method
-    myCar.speed(200);          // Call the speed() method
+    System.out.println(myMethod(5, 3));
   }
 }
